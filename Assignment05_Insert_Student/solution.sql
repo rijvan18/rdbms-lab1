0141334101781e0677e97cef5bedb80eea0340f6
rijvan18 <mohammedrijvan93600@gmail.com>
2026-08-27 12:13:17 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -7,3 +7,16 @@
 -- 1003 Karthik
 
 -- Display all records
+
+CREATE TABLE Student(
+studentID INT PRIMARY KEY,
+studentName VARCHAR(50),
+Gender VARCHAR(10),
+DeaprtmentID INT);
+
+INSERT INTO Student (studentID, StudentName, Gender, DepartmentID)
+VALUES (1001, 'Arun', 'Male', 101),
+(1002, 'Divya', 'Feamle', 102),
+(1003, 'Karthick', 'Male', 101);
+
+SELECT*FROM Student
