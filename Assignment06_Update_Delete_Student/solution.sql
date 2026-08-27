aef93c12ddf3497347cdb8dc08a2c1e582108ebc
rijvan18 <mohammedrijvan93600@gmail.com>
2026-08-27 12:14:22 +0530

    Update solution.sql

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -9,3 +9,23 @@
 -- Delete StudentID 1002
 
 -- Display all records
+
+CREATE TABLE student(
+        studentID INT PRIMARY KEY,
+        studentName VARCHAR(50),
+        deaprtment_id INT
+        );
+        
+        INSERT INTO student VALUES (1001,'Arun',101);
+        INSERT INTO student VALUES (1002,'kUMAR',102);
+        INSERT INTO student VALUES (1003,'Karthik',101);
+        INSERT INTO student VALUES (1004,'Ravi',104);
+    
+    UPDATE student
+    SET department_id = 103
+    WHERE studentName = 'Kathik';
+    
+    DELETE FROM student
+    WHERE studentID = 1002;
+    
+    SELECT*FROM student;
