0a9ec8a381302ce3ae686cbf7b9e38217629a5da
rijvan18 <mohammedrijvan93600@gmail.com>
2026-08-27 12:41:19 +0530

    Update solution.sql

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -8,24 +8,11 @@
 
 -- Delete StudentID 1002
 
--- Display all records
+-- Display all records-- Update Karthik's department to 103
+UPDATE Student
+SET DepartmentID = 103
+WHERE StudentName = 'Karthik';
 
-CREATE TABLE student(
-        studentID INT PRIMARY KEY,
-        studentName VARCHAR(50),
-        deaprtment_id INT
-        );
-        
-        INSERT INTO student VALUES (1001,'Arun',101);
-        INSERT INTO student VALUES (1002,'kUMAR',102);
-        INSERT INTO student VALUES (1003,'Karthik',101);
-        INSERT INTO student VALUES (1004,'Ravi',104);
-    
-    UPDATE student
-    SET department_id = 103
-    WHERE studentName = 'Kathik';
-    
-    DELETE FROM student
-    WHERE studentID = 1002;
-    
-    SELECT*FROM student;
+-- Delete record for StudentID 1002
+DELETE FROM Student
+WHERE StudentID = 1002;
